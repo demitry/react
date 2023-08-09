81a826f9f6f7b9f6f4fa16b4360c31e479ffb5d2
demitry <dpoluektov@gmail.com>
2023-08-09 19:21:58 +0300

    clean up

--- Source/03-react-basics/first-app/src/App.js
+++ Source/03-react-basics/first-app/src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 import Product  from './components/Product';
 
 function App() {
+  
   const expenses = [
     { id: 'e1', title: 'Car Insurance', amount: 294.57, date: new Date(2023, 2, 28) },
     { id: 'e2', title: 'Paper', amount: 123.12, date: new Date(2023, 3, 27) },
@@ -15,36 +15,19 @@ function App() {
     { title:'Product 1', price: 10, description: 'First product' },
     { title:'Product 2', price: 20, description: 'Second product' }
   ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>This is also visible, Wow, on the fly!</p>
-
-        <Product title={products[0].title} price={products[0].price} description={products[0].description}></Product>
-        <Product title={products[1].title} price={products[1].price} description={products[1].description}></Product>
-
-        {/* <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-        <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-        <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-        <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem> */}
 
         <ExpenseItem expense={expenses[0]}></ExpenseItem>
         <ExpenseItem expense={expenses[1]}></ExpenseItem>
         <ExpenseItem expense={expenses[2]}></ExpenseItem>
         <ExpenseItem expense={expenses[3]}></ExpenseItem>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Product title={products[0].title} price={products[0].price} description={products[0].description}></Product>
+        <Product title={products[1].title} price={products[1].price} description={products[1].description}></Product>
+
       </header>
     </div>
   );
